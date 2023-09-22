3a4ad57f70de2add91e8476887a652ef66e2f26e
Anders Østby <anders.ostby@nav.no>
2023-09-22 11:06:23 +0200

    Fjerner clever-implementasjonen av konsistentsjekken til SykdomsGrunnlag

--- src/main/kotlin/no/nav/aap/domene/behandling/grunnlag/sykdom/SykdomsGrunnlag.kt
+++ src/main/kotlin/no/nav/aap/domene/behandling/grunnlag/sykdom/SykdomsGrunnlag.kt
@@ -11,13 +11,9 @@ class SykdomsGrunnlag(
     val sykdomsvurdering: Sykdomsvurdering
 ) {
     fun erKonsistent(): Boolean {
-        //TODO: Clever or not??
-        val clever = true
-        if (clever) {
-            return (yrkesskadevurdering?.erÅrsakssammenheng == true) == (sykdomsvurdering.nedreGrense == NedreGrense.TRETTI)
+        if (yrkesskadevurdering?.erÅrsakssammenheng == true) {
+            return sykdomsvurdering.nedreGrense == NedreGrense.TRETTI
         }
-
-        if (yrkesskadevurdering?.erÅrsakssammenheng == true) return sykdomsvurdering.nedreGrense == NedreGrense.TRETTI
         return sykdomsvurdering.nedreGrense == NedreGrense.FEMTI
     }
 }
