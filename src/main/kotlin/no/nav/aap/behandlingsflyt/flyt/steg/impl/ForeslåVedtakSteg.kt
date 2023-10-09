1f1ceadee332d22391b696f6b26059600601b1ec
Marius Glittum <marius.glittum@nav.no>
2023-10-09 10:24:27 +0200

    Avbryter foreslå vedtak ved tilbakehopp
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Terje Heen <terje.heen@nav.no

--- src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/steg/impl/ForeslåVedtakSteg.kt
+++ src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/steg/impl/ForeslåVedtakSteg.kt
@@ -4,10 +4,10 @@ import no.nav.aap.behandlingsflyt.domene.behandling.Behandling
 import no.nav.aap.behandlingsflyt.domene.behandling.BehandlingTjeneste
 import no.nav.aap.behandlingsflyt.domene.behandling.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.domene.behandling.avklaringsbehov.Status
-import no.nav.aap.behandlingsflyt.flyt.steg.StegType
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.StegInput
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
+import no.nav.aap.behandlingsflyt.flyt.steg.StegType
 
 class ForeslåVedtakSteg : BehandlingSteg {
     override fun utfør(input: StegInput): StegResultat {
@@ -32,5 +32,16 @@ class ForeslåVedtakSteg : BehandlingSteg {
 
     override fun type(): StegType {
         return StegType.FORESLÅ_VEDTAK
+    }
+
+    override fun vedTilbakeføring(input: StegInput) {
+        val behandling = BehandlingTjeneste.hent(input.kontekst.behandlingId)
+        val avklaringsbehovene = behandling.avklaringsbehovene()
+        val relevanteBehov =
+            avklaringsbehovene.hentBehovForLøsninger(listOf(Definisjon.FORESLÅ_VEDTAK))
+
+        if (relevanteBehov.isNotEmpty()) {
+            avklaringsbehovene.avbryt(Definisjon.FORESLÅ_VEDTAK)
+        }
     }
 }
