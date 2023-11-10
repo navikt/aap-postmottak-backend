a1ba679986bbaecc99b96f45d44e9fedb128c9f2
Marius Glittum <marius.glittum@nav.no>
2023-11-10 10:28:57 +0100

    Legger til mulighet for å sette query timeout og setter default verdi

--- src/main/kotlin/no/nav/aap/behandlingsflyt/dbstuff/Query.kt
+++ src/main/kotlin/no/nav/aap/behandlingsflyt/dbstuff/Query.kt
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement
 
 class Query<T>(private val preparedStatement: PreparedStatement) {
     private lateinit var rowMapper: (Row) -> T
+    private var queryTimeout = 30
 
     fun setParams(block: Params.() -> Unit) {
         Params(preparedStatement).block()
@@ -13,11 +14,22 @@ class Query<T>(private val preparedStatement: PreparedStatement) {
         rowMapper = block
     }
 
+    fun setQueryTimeout(sekunder: Int) {
+        validering(sekunder)
+        queryTimeout = sekunder
+    }
+
+
     fun executeQuery(): Sequence<T> {
         val resultSet = preparedStatement.executeQuery()
+        preparedStatement.queryTimeout = queryTimeout
         return resultSet
             .map { currentResultSet ->
                 rowMapper(Row(currentResultSet))
             }
     }
+}
+
+internal fun validering(sekunder: Int) {
+    require(sekunder in 300 downTo 1)
 }
