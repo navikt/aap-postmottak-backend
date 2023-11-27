f3d23b59e93ff5c863b2d562be789936dff44b8a
Anders Østby <anders.ostby@nav.no>
2023-11-27 13:28:30 +0100

    Bumper avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import java.io.ByteArrayOutputStream
 
-val ktorVersion = "2.3.5"
+val ktorVersion = "2.3.6"
 
 plugins {
-    kotlin("jvm") version "1.9.20"
-    id("io.ktor.plugin") version "2.3.5"
+    kotlin("jvm") version "1.9.21"
+    id("io.ktor.plugin") version "2.3.6"
 }
 
 application {
@@ -85,21 +85,21 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktorVersion")
 
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
     implementation("dev.forst:ktor-openapi-generator:0.6.1")
 
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.11.4")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.0")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.12.0")
     implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("net.logstash.logback:logstash-logback-encoder:7.4")
 
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:9.22.3")
-    runtimeOnly("org.postgresql:postgresql:42.6.0")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("org.flywaydb:flyway-database-postgresql:10.1.0")
+    runtimeOnly("org.postgresql:postgresql:42.7.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.testcontainers:postgresql:1.19.1")
+    testImplementation("org.testcontainers:postgresql:1.19.3")
     testImplementation(kotlin("test"))
 }
