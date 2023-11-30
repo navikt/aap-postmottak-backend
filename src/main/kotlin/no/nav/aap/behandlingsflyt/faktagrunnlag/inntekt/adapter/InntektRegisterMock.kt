99e425051b57db85d23a3488f585bb9548f48d7b
Marius Glittum <marius.glittum@nav.no>
2023-11-30 16:56:54 +0100

    Hvis ingen inntekter så lagres 0 per år vi henter inn
    Co-authored-by: Anders Østby <anders.ostby@nav.no>

--- src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/inntekt/adapter/InntektRegisterMock.kt
+++ src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/inntekt/adapter/InntektRegisterMock.kt
@@ -1,18 +1,27 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.inntekt.adapter
 
+import no.nav.aap.behandlingsflyt.faktagrunnlag.inntekt.Beløp
 import no.nav.aap.behandlingsflyt.sak.Ident
 import java.time.Year
 
 object InntektRegisterMock {
-    private val skader = HashMap<Ident, List<InntektPerÅr>>()
+    private val inntekter = HashMap<Ident, List<InntektPerÅr>>()
 
     fun innhent(identer: List<Ident>, år: Set<Year>): Set<InntektPerÅr> {
-        return skader.filter { entry -> identer.contains(entry.key) }
-            .flatMap { entry -> entry.value.filter { år.contains(it.år) } }
-            .toSortedSet()
+        val resultat: MutableSet<InntektPerÅr> = mutableSetOf()
+        for (year in år) {
+            val innhentedeInntekter = inntekter.filter { entry -> identer.contains(entry.key) }
+                .flatMap { entry -> entry.value.filter { år.contains(year) } }
+                .toSortedSet()
+
+            val summerteInntekter = innhentedeInntekter.sumOf { it.beløp.verdi() }
+
+            resultat.add(InntektPerÅr(year, Beløp(summerteInntekter)))
+        }
+        return resultat.toSortedSet()
     }
 
     fun konstruer(ident: Ident, inntekterPerÅr: List<InntektPerÅr>) {
-        skader[ident] = inntekterPerÅr
+        inntekter[ident] = inntekterPerÅr
     }
 }
