90b45259445ceb34152ae317b0ec5698477748b3
Anders Østby <anders.ostby@nav.no>
2024-01-17 12:03:30 +0100

    dbconnect skal ha testavhengighet til dbtest

--- dbconnect/build.gradle.kts
+++ dbconnect/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     implementation(project(":verdityper"))
-    implementation(project(":dbtest"))
 
+    testImplementation(project(":dbtest"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     testImplementation("org.assertj:assertj-core:3.25.1")
