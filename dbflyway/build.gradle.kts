cc457c084e37b41e5f57651b0cef6d3d000fb6ba
Anders Østby <anders.ostby@nav.no>
2024-01-29 14:22:18 +0100

    Fjerner unødvendige avhengigheter

--- dbflyway/build.gradle.kts
+++ dbflyway/build.gradle.kts
@@ -1,8 +1,5 @@
 dependencies {
-    implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("org.flywaydb:flyway-core:10.6.0")
     implementation("org.flywaydb:flyway-database-postgresql:10.6.0")
     runtimeOnly("org.postgresql:postgresql:42.7.1")
-
-    implementation("org.testcontainers:postgresql:1.19.3")
 }
