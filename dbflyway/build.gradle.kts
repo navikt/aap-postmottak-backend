dd9f19b4f6c5a047c050ca3ae97646f498fa600e
Anders Østby <anders.ostby@nav.no>
2024-02-15 08:01:53 +0100

    Bumper gradle-build-action og flyway

--- dbflyway/build.gradle.kts
+++ dbflyway/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation("org.flywaydb:flyway-core:10.7.1")
-    implementation("org.flywaydb:flyway-database-postgresql:10.7.1")
+    implementation("org.flywaydb:flyway-core:10.7.2")
+    implementation("org.flywaydb:flyway-database-postgresql:10.7.2")
     runtimeOnly("org.postgresql:postgresql:42.7.1")
 }
--- sakogbehandling/build.gradle.kts
+++ sakogbehandling/build.gradle.kts
@@ -8,8 +8,8 @@ dependencies {
     implementation(project(":httpklient"))
 
     implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("org.flywaydb:flyway-core:10.7.1")
-    implementation("org.flywaydb:flyway-database-postgresql:10.7.1")
+    implementation("org.flywaydb:flyway-core:10.7.2")
+    implementation("org.flywaydb:flyway-database-postgresql:10.7.2")
     runtimeOnly("org.postgresql:postgresql:42.7.1")
 
     testImplementation(project(":dbtestdata"))
