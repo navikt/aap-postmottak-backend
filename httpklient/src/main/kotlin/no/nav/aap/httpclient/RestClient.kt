0c789ad8501d59c2e11108d504f4f0ea808103cc
Marius Glittum <marius.glittum@nav.no>
2024-04-08 09:28:45 +0200

    Sender access_token ikke objects tostring

--- httpklient/src/main/kotlin/no/nav/aap/httpclient/RestClient.kt
+++ httpklient/src/main/kotlin/no/nav/aap/httpclient/RestClient.kt
@@ -94,7 +94,7 @@ private fun HttpRequest.Builder.addHeaders(
 
     val token = tokenProvider.getToken(scope)
     if (token != null) {
-        this.header("Authorization", "Bearer $token")
+        this.header("Authorization", "Bearer ${token.accessToken}")
     }
     val callId = sikreCorrelationId()
     this.header("X-Correlation-ID", callId)
