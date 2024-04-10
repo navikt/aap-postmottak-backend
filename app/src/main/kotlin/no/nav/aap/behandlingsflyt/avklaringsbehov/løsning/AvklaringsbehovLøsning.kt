2a174e4ecf61c0b0bf7517d89457cecb0473c425
Marius Glittum <marius.glittum@nav.no>
2024-04-10 10:06:12 +0200

    Fjerner annotasjon fra interface

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/løsning/AvklaringsbehovLøsning.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/løsning/AvklaringsbehovLøsning.kt
@@ -2,14 +2,12 @@ package no.nav.aap.behandlingsflyt.avklaringsbehov.løsning
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeName
-import com.papsign.ktor.openapigen.annotations.type.string.example.DiscriminatorAnnotation
 import no.nav.aap.behandlingsflyt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.LøsningsResultat
 import no.nav.aap.behandlingsflyt.dbconnect.DBConnection
 import no.nav.aap.verdityper.flyt.FlytKontekst
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "behovstype", visible = true)
-@DiscriminatorAnnotation(fieldName = "behovstype")
 sealed interface AvklaringsbehovLøsning {
     fun definisjon(): Definisjon {
         if (this.javaClass.isAnnotationPresent(JsonTypeName::class.java)) {
