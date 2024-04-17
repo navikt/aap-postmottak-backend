01aa68dbe8ff683f0c7b5eda3e89d5284d449d29
Marius Glittum <marius.glittum@nav.no>
2024-04-17 13:32:00 +0200

    JsonValue på referanse

--- sakogbehandling/src/main/kotlin/no.nav.aap.behandlingsflyt/sakogbehandling/behandling/flate/BehandlingReferanse.kt
+++ sakogbehandling/src/main/kotlin/no.nav.aap.behandlingsflyt/sakogbehandling/behandling/flate/BehandlingReferanse.kt
@@ -1,10 +1,16 @@
 package no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate
 
+import com.fasterxml.jackson.annotation.JsonValue
 import com.papsign.ktor.openapigen.annotations.parameters.PathParam
 import java.util.*
 
 data class BehandlingReferanse(@PathParam("referanse") val referanse: String) {
     fun ref(): UUID {
         return UUID.fromString(referanse)
+    }
+
+    @JsonValue
+    override fun toString(): String {
+        return referanse
     }
 }
