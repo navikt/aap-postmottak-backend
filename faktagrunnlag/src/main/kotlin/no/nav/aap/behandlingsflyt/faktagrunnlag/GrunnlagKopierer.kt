7d783bd1f7bf9f10993297601191fb3cef15e28d
Marius Glittum <marius.glittum@nav.no>
2024-05-27 11:13:21 +0200

    Legger til manglende kopier ved opprettelse av revurdering

--- faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/GrunnlagKopierer.kt
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/GrunnlagKopierer.kt
@@ -1,14 +1,19 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag
 
 import no.nav.aap.behandlingsflyt.dbconnect.DBConnection
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.barnetillegg.BarnetilleggRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.beregning.BeregningsgrunnlagRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.VilkårsresultatRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid.PliktkortRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.Institusjonsopphold.InstitusjonsoppholdRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.barn.BarnRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.PersonopplysningRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.uføre.UføreRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.yrkesskade.YrkesskadeRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.arbeidsevne.ArbeidsevneRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.barn.BarnVurderingRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.BeregningVurderingRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.bistand.BistandRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentRepository
@@ -16,6 +21,11 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykdomRepos
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykepengerErstatningRepository
 import no.nav.aap.verdityper.sakogbehandling.BehandlingId
 
+/**
+ * Har som ansvar å sette i stand en behandling etter opprettelse
+ *
+ * Knytter alle opplysninger fra forrige til den nye i en immutable state
+ */
 class GrunnlagKopierer(connection: DBConnection) {
 
     private val vilkårsresultatRepository: VilkårsresultatRepository = VilkårsresultatRepository(connection)
@@ -31,6 +41,11 @@ class GrunnlagKopierer(connection: DBConnection) {
     private val pliktkortRepository = PliktkortRepository(connection)
     private val underveisRepository = UnderveisRepository(connection)
     private val barnRepository = BarnRepository(connection)
+    private val barnetilleggRepository = BarnetilleggRepository(connection)
+    private val barnVurderingRepository = BarnVurderingRepository(connection)
+    private val beregningsgrunnlagRepository = BeregningsgrunnlagRepository(connection)
+    private val beregningVurderingRepository = BeregningVurderingRepository(connection)
+    private val institusjonsoppholdRepository = InstitusjonsoppholdRepository(connection)
 
     fun overfør(fraBehandlingId: BehandlingId, tilBehandlingId: BehandlingId) {
         require(fraBehandlingId != tilBehandlingId)
@@ -48,5 +63,10 @@ class GrunnlagKopierer(connection: DBConnection) {
         pliktkortRepository.kopier(fraBehandlingId, tilBehandlingId)
         underveisRepository.kopier(fraBehandlingId, tilBehandlingId)
         barnRepository.kopier(fraBehandlingId, tilBehandlingId)
+        barnetilleggRepository.kopier(fraBehandlingId, tilBehandlingId)
+        barnVurderingRepository.kopier(fraBehandlingId, tilBehandlingId)
+        beregningVurderingRepository.kopier(fraBehandlingId, tilBehandlingId)
+        beregningsgrunnlagRepository.kopier(fraBehandlingId, tilBehandlingId)
+        institusjonsoppholdRepository.kopier(fraBehandlingId, tilBehandlingId)
     }
 }
