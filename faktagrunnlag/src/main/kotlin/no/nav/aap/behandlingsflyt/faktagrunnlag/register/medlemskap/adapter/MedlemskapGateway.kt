5859c9dac8477f2c89a5c2b4a76183f59fbced4f
Henrik Jackson <henrik.m.jackson@gmail.com>
2024-07-03 09:52:51 +0200

    Gateway for medlemskap - mangler innhent-fun

---
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/medlemskap/adapter/MedlemskapGateway.kt
@@ -0,0 +1,46 @@
+package no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.adapter
+
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.Medlemskap
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.MedlemskapGateway
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Person
+import no.nav.aap.httpclient.ClientConfig
+import no.nav.aap.httpclient.Header
+import no.nav.aap.httpclient.RestClient
+import no.nav.aap.httpclient.get
+import no.nav.aap.httpclient.request.GetRequest
+import no.nav.aap.httpclient.tokenprovider.azurecc.ClientCredentialsTokenProvider
+import no.nav.aap.medlemskap.MedlemskapRequest
+import no.nav.aap.medlemskap.MedlemskapResponse
+import no.nav.aap.requiredConfigForKey
+import java.net.URI
+
+object MedlemskapGateway : MedlemskapGateway{
+    private val url = URI.create(requiredConfigForKey("integrasjon.medl.url"))
+    val config = ClientConfig(scope = requiredConfigForKey("integrasjon.medl.scope"))
+
+    private val client = RestClient.withDefaultResponseHandler(
+        config = config,
+        tokenProvider = ClientCredentialsTokenProvider
+    )
+
+    private fun query(request: MedlemskapRequest): MedlemskapResponse {
+        val httpRequest = GetRequest(
+            additionalHeaders = listOf(
+                Header("Nav-Consumer-Id", "aap-behandlingsflyt"),
+                Header("Nav-Personident", request.fodselsnumre.first()),
+                Header("Accept", "application/json"),
+            )
+        )
+
+        return requireNotNull(client.get(uri = url, request = httpRequest))
+    }
+
+    override fun innhent(person: Person): List<Medlemskap> {
+        TODO("Not yet implemented")
+        val request = MedlemskapRequest(person.identer().map { it.identifikator })
+        val medlemskapResultat = query(request)
+
+        return emptyList()
+    }
+
+}
