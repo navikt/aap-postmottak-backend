138ca2c81521b46b9c4d4a76299fe6ba649ff79c
AlexKjes <alexander.kjeserud@nav.no>
2024-09-13 10:56:41 +0200

    :bug: Retter opp dokument info id i Dokument dto

--- verdityper/src/main/kotlin/no/nav/aap/verdityper/dokument/DokumentInfoId.kt
+++ verdityper/src/main/kotlin/no/nav/aap/verdityper/dokument/DokumentInfoId.kt
@@ -1,3 +1,7 @@
 package no.nav.aap.verdityper.dokument
 
-data class DokumentInfoId(val dokumentInfoId: String)
+data class DokumentInfoId(val dokumentInfoId: String) {
+    override fun toString(): String {
+        return dokumentInfoId
+    }
+}
