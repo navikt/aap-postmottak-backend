37503f8f94698c0833d1bae7656cb07b228d37a9
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2024-09-30 13:23:58 +0200

    :sparkles: Visningsutledere for 'overelever til fagsystem' og 'digitaliser'

---
+++ app/src/test/kotlin/no/nav/aap/postmottak/flyt/DynamiskStegGruppeVisningService.kt
@@ -0,0 +1,60 @@
+package no.nav.aap.postmottak.flyt
+
+import no.nav.aap.komponenter.dbconnect.transaction
+import no.nav.aap.komponenter.dbtest.InitTestDatabase
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.JournalpostRepositoryImpl
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.adapters.saf.Dokument
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.adapters.saf.Filtype
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.adapters.saf.Ident
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.adapters.saf.Journalpost
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.adapters.saf.JournalpostStatus
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.adapters.saf.Variantformat
+import no.nav.aap.postmottak.flyt.flate.visning.DynamiskStegGruppeVisningService
+import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
+import no.nav.aap.postmottak.kontrakt.steg.StegGruppe
+import no.nav.aap.postmottak.sakogbehandling.behandling.BehandlingRepositoryImpl
+import no.nav.aap.verdityper.dokument.DokumentInfoId
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class DynamiskStegGruppeVisningServiceTest {
+    val journalpost = Journalpost.MedIdent(
+        journalpostId = JournalpostId(111),
+        journalførendeEnhet = null,
+        status = JournalpostStatus.MOTTATT,
+        dokumenter = listOf(
+            Dokument(
+                tittel = "Søknad",
+                brevkode = "NAV 11-13.05",
+                filtype = Filtype.JSON,
+                variantFormat = Variantformat.ORIGINAL,
+                dokumentInfoId = DokumentInfoId("1")
+            )
+        ),
+        personident = Ident.Personident("12345678901"),
+        mottattDato = LocalDate.of(2021, 1, 1)
+
+    )
+
+    @Test
+    fun `Skal vise steg 'overlever til fagsystem' for søknad`() {
+        InitTestDatabase.dataSource.transaction { connection ->
+            // Arrange
+            val journalpostRepository = JournalpostRepositoryImpl(connection)
+            val behandlingRepository = BehandlingRepositoryImpl(connection)
+            val behandlingId = behandlingRepository.opprettBehandling(journalpost.journalpostId).id
+            journalpostRepository.lagre(journalpost, behandlingId)
+
+            val service = DynamiskStegGruppeVisningService(connection)
+            
+            val gruppe = StegGruppe.OVERLEVER_TIL_FAGSYSTEM
+
+            // Act
+            val result = service.skalVises(gruppe, behandlingId)
+
+            // Assert
+            assertTrue(result)
+        }
+    }
+}
--- kontrakt/src/main/kotlin/no/nav/aap/postmottak/kontrakt/steg/StegGruppe.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/postmottak/kontrakt/steg/StegGruppe.kt
@@ -2,12 +2,12 @@ package no.nav.aap.postmottak.kontrakt.steg
 
 enum class StegGruppe(val skalVises: Boolean, val obligatoriskVisning: Boolean) {
     KATEGORISER(true, true),
-    DIGITALISER(true, true),
+    DIGITALISER(true, false),
     AVKLAR_TEMA(true, true),
     START_BEHANDLING(false, false),
     UDEFINERT(false, true),
     SETT_FAGSAK(false, false),
     ENDELIG_JOURNALFØRING(false, false),
-    OVERLEVER_TIL_FAGSYSTEM(true, true),
+    OVERLEVER_TIL_FAGSYSTEM(true, false),
     AVKLAR_SAK(true, true)
 }
