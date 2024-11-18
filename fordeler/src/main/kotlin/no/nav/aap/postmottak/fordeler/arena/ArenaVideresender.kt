4efb21f10bdfcd16a46e8013b0594e441f92349d
AlexKjes <alexander.kjeserud@nav.no>
2024-11-18 11:56:16 +0100

    :loud_sound: LEgger på logging i videresender

--- fordeler/src/main/kotlin/no/nav/aap/postmottak/fordeler/arena/ArenaVideresender.kt
+++ fordeler/src/main/kotlin/no/nav/aap/postmottak/fordeler/arena/ArenaVideresender.kt
@@ -3,6 +3,7 @@ package no.nav.aap.postmottak.fordeler.arena
 import no.nav.aap.postmottak.fordeler.HendelsesRepository
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.LoggerFactory
 
 const val ARENA_VIDERESEND_TOPIC = "aap.journalpost-til-arena"
 
@@ -12,9 +13,12 @@ class ArenaVideresender(
     private val producer: Producer<String, String>, private val hendelsesRepository: HendelsesRepository
 ) {
 
+    private val log = LoggerFactory.getLogger(ArenaVideresender::class.java)
+
     fun sendJournalpostTilArena(hendelsesId: HendelsesId) {
         val hendelse = hendelsesRepository.hentHendelse(hendelsesId)
         val record = ProducerRecord(ARENA_VIDERESEND_TOPIC, hendelse.hendelsesid, hendelse.hendelse)
+        log.info("Videresender medling $hendelsesId fra Joark til Arena")
         producer.send(record)
     }
 }
