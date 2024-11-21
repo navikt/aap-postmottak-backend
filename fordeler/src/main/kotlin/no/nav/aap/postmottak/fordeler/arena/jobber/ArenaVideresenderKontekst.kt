d7d11745f3977c97fa69535413b747898b1dbbb4
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2024-11-21 17:18:25 +0100

    :white_check_mark: Legg til test for SendSøknadTilArenaJobb, fiks bug i setting av payload i ArenaVideresenderKontekst

--- fordeler/src/main/kotlin/no/nav/aap/postmottak/fordeler/arena/jobber/ArenaVideresenderKontekst.kt
+++ fordeler/src/main/kotlin/no/nav/aap/postmottak/fordeler/arena/jobber/ArenaVideresenderKontekst.kt
@@ -1,6 +1,6 @@
 package no.nav.aap.postmottak.fordeler.arena.jobber
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import no.nav.aap.komponenter.httpklient.json.DefaultJsonMapper
 import no.nav.aap.motor.JobbInput
 import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
 import no.nav.aap.verdityper.sakogbehandling.Ident
@@ -11,13 +11,10 @@ data class ArenaVideresenderKontekst(
     val ident: Ident,
     val hoveddokumenttittel: String,
     val vedleggstittler: List<String>,
-    val navEnhet: String,
+    val navEnhet: String
 )
 
-private val objectMapper = ObjectMapper()
-
-fun JobbInput.getArenaVideresenderKontekst() = objectMapper.readValue(this.payload(), ArenaVideresenderKontekst::class.java)
+fun JobbInput.getArenaVideresenderKontekst() = DefaultJsonMapper.fromJson(this.payload(), ArenaVideresenderKontekst::class.java)
 fun JobbInput.medArenaVideresenderKontekst(arenaVideresender: ArenaVideresenderKontekst): JobbInput {
-    objectMapper.writeValueAsString(arenaVideresender)
-    return this
+    return this.medPayload(DefaultJsonMapper.toJson(arenaVideresender))
 }
---
+++ fordeler/src/test/kotlin/no/nav/aap/postmottak/fordeler/arena/jobber/SendSøknadTilArenaJobbTest.kt
@@ -0,0 +1,47 @@
+package no.nav.aap.postmottak.fordeler.arena.jobber
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.aap.komponenter.httpklient.json.DefaultJsonMapper
+import no.nav.aap.motor.FlytJobbRepository
+import no.nav.aap.motor.JobbInput
+import no.nav.aap.postmottak.klient.arena.ArenaKlient
+import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
+import no.nav.aap.postmottak.test.fakes.WithFakes
+import no.nav.aap.verdityper.sakogbehandling.Ident
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SendSøknadTilArenaJobbTest: WithFakes {
+    val flytJobbRepositoryMock = mockk<FlytJobbRepository>(relaxed = true)
+    val arenaKlientMock = mockk<ArenaKlient>(relaxed = true)
+    val sendSøknadTilArenaJobb = SendSøknadTilArenaJobb(flytJobbRepositoryMock, arenaKlientMock)
+
+    @Test
+    fun `Skal opprette jobb for manuell oppgave dersom det ikke finnes aktiv sak`() {
+        every {arenaKlientMock.harAktivSak(any())} returns false
+        
+        val journalpostId = JournalpostId(1)
+        
+        val jobbKontekst =   ArenaVideresenderKontekst(
+            journalpostId = journalpostId,
+            ident = Ident("123"),
+            hoveddokumenttittel = "Hoveddokument",
+            vedleggstittler = listOf("Vedlegg"),
+            navEnhet = "4491"
+        )
+        
+        val jobbInput = JobbInput(SendSøknadTilArenaJobb).medArenaVideresenderKontekst(
+            jobbKontekst
+        )
+        sendSøknadTilArenaJobb.utfør(jobbInput)
+        
+        verify(exactly = 1) {flytJobbRepositoryMock.leggTil(withArg{
+            assertThat(it.type()).isEqualTo(ManuellJournalføringsoppgavejobb.type())
+            val actualKontekst = DefaultJsonMapper.fromJson<ArenaVideresenderKontekst>(it.payload())
+            assertThat(actualKontekst).isEqualTo(jobbKontekst)
+        })}
+            
+    }
+}
