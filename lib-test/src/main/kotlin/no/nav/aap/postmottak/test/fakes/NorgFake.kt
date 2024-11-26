963f8151ecfb69c20acaa08d104daf61c0567b49
AlexKjes <alexander.kjeserud@nav.no>
2024-11-26 08:50:05 +0100

    :bug: Glemmer atter en gang å opopdatere fake

--- lib-test/src/main/kotlin/no/nav/aap/postmottak/test/fakes/NorgFake.kt
+++ lib-test/src/main/kotlin/no/nav/aap/postmottak/test/fakes/NorgFake.kt
@@ -8,7 +8,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 val finnEnhet: suspend RoutingContext.() -> Unit = {
-    call.respond("""{"enhetNr": "superNav!"}""")
+    call.respond("""[{"enhetNr": "superNav!"}]""")
 }
 
 fun Application.norgFake(
