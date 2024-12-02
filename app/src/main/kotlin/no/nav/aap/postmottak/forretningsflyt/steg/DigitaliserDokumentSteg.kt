0e5e70cdfbfce8e804a3dddb54c4749f2fef6b09
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2024-12-02 09:46:54 +0100

    :sparkles: Hopp over digitaliseringssteg for kategorier som ikke skal digitaliseres

--- app/src/main/kotlin/no/nav/aap/postmottak/forretningsflyt/steg/DigitaliserDokumentSteg.kt
+++ app/src/main/kotlin/no/nav/aap/postmottak/forretningsflyt/steg/DigitaliserDokumentSteg.kt
@@ -1,8 +1,12 @@
 package no.nav.aap.postmottak.forretningsflyt.steg
 
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.InnsendingType
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.JournalpostRepository
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.JournalpostRepositoryImpl
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.kategorisering.KategoriVurdering
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.kategorisering.KategorivurderingRepository
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.strukturering.Struktureringsvurdering
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.strukturering.StruktureringsvurderingRepository
 import no.nav.aap.postmottak.flyt.steg.BehandlingSteg
 import no.nav.aap.postmottak.flyt.steg.FantAvklaringsbehov
@@ -11,17 +15,20 @@ import no.nav.aap.postmottak.flyt.steg.Fullført
 import no.nav.aap.postmottak.flyt.steg.StegResultat
 import no.nav.aap.postmottak.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.postmottak.kontrakt.steg.StegType
+import no.nav.aap.postmottak.sakogbehandling.journalpost.Journalpost
 import no.nav.aap.verdityper.flyt.FlytKontekstMedPerioder
 
 class DigitaliserDokumentSteg(
     private val struktureringsvurderingRepository: StruktureringsvurderingRepository,
-    private val journalpostRepository: JournalpostRepository
+    private val journalpostRepository: JournalpostRepository,
+    private val kategorivurderingRepository: KategorivurderingRepository
 ) : BehandlingSteg {
     companion object : FlytSteg {
         override fun konstruer(connection: DBConnection): BehandlingSteg {
             return DigitaliserDokumentSteg(
                 StruktureringsvurderingRepository(connection),
-                JournalpostRepositoryImpl(connection)
+                JournalpostRepositoryImpl(connection),
+                KategorivurderingRepository(connection)
             )
         }
 
@@ -34,13 +41,28 @@ class DigitaliserDokumentSteg(
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
         val struktureringsvurdering =
             struktureringsvurderingRepository.hentStruktureringsavklaring(kontekst.behandlingId)
+        val kategorivurdering = kategorivurderingRepository.hentKategoriAvklaring(kontekst.behandlingId)
         val journalpost = journalpostRepository.hentHvisEksisterer(kontekst.behandlingId)
 
         requireNotNull(journalpost)
 
-        return if (!journalpost.kanBehandlesAutomatisk() && struktureringsvurdering == null) FantAvklaringsbehov(
+        return if (skalDigitaliseres(journalpost, struktureringsvurdering, kategorivurdering)) FantAvklaringsbehov(
             Definisjon.DIGITALISER_DOKUMENT
         )
         else Fullført
+    }
+
+    private fun skalDigitaliseres(
+        journalpost: Journalpost,
+        struktureringsvurdering: Struktureringsvurdering?,
+        kategorivurdering: KategoriVurdering?
+    ): Boolean {
+        return !journalpost.kanBehandlesAutomatisk() 
+                && struktureringsvurdering == null 
+                && kategorivurdering?.avklaring?.kanStruktureres()!!
+    }
+
+    private fun InnsendingType.kanStruktureres(): Boolean {
+        return this in listOf(InnsendingType.SØKNAD, InnsendingType.PLIKTKORT, InnsendingType.AKTIVITETSKORT)
     }
 }
--- app/src/test/kotlin/no/nav/aap/postmottak/forretningsflyt/steg/DigitaliserDokumentStegTest.kt
+++ app/src/test/kotlin/no/nav/aap/postmottak/forretningsflyt/steg/DigitaliserDokumentStegTest.kt
@@ -2,23 +2,29 @@ package no.nav.aap.postmottak.forretningsflyt.steg
 
 import io.mockk.every
 import io.mockk.mockk
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.InnsendingType
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.JournalpostRepositoryImpl
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.kategorisering.KategoriVurdering
+import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.kategorisering.KategorivurderingRepository
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.strukturering.StruktureringsvurderingRepository
+import no.nav.aap.postmottak.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.postmottak.flyt.steg.Fullført
 import no.nav.aap.postmottak.flyt.steg.FunnetAvklaringsbehov
 import no.nav.aap.postmottak.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.postmottak.sakogbehandling.journalpost.Journalpost
 import no.nav.aap.verdityper.sakogbehandling.BehandlingId
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class DigitaliserDokumentStegTest {
 
     val struktureringsvurderingRepository: StruktureringsvurderingRepository = mockk()
     val journalpostRepo: JournalpostRepositoryImpl = mockk()
+    val kategorivurderingRepo: KategorivurderingRepository = mockk()
 
     val digitaliserDokumentSteg = DigitaliserDokumentSteg(
-        struktureringsvurderingRepository, journalpostRepo
+        struktureringsvurderingRepository, journalpostRepo, kategorivurderingRepo
     )
 
     @Test
@@ -28,9 +34,11 @@ class DigitaliserDokumentStegTest {
         every { journalpost.kanBehandlesAutomatisk() } returns false
         every { struktureringsvurderingRepository.hentStruktureringsavklaring(any()) } returns null
         every { journalpostRepo.hentHvisEksisterer(any<BehandlingId>()) } returns journalpost
+        every { kategorivurderingRepo.hentKategoriAvklaring(any()) } returns KategoriVurdering(InnsendingType.SØKNAD)
 
         val stegresultat = digitaliserDokumentSteg.utfør(mockk(relaxed = true))
 
+        assertEquals(stegresultat::class.simpleName, FantAvklaringsbehov::class.simpleName)
         val funnetAvklaringsbehov = stegresultat.transisjon() as FunnetAvklaringsbehov
         assertThat(funnetAvklaringsbehov.avklaringsbehov()).contains(Definisjon.DIGITALISER_DOKUMENT)
 
@@ -43,10 +51,11 @@ class DigitaliserDokumentStegTest {
         every { journalpost.kanBehandlesAutomatisk() } returns false
         every { struktureringsvurderingRepository.hentStruktureringsavklaring(any()) } returns mockk(relaxed = true)
         every { journalpostRepo.hentHvisEksisterer(any<BehandlingId>()) } returns journalpost
+        every { kategorivurderingRepo.hentKategoriAvklaring(any()) } returns mockk(relaxed = true)
 
         val stegresultat = digitaliserDokumentSteg.utfør(mockk(relaxed = true))
-
-        assertThat(stegresultat is Fullført)
+        
+        assertEquals(stegresultat::class.simpleName, Fullført::class.simpleName)
     }
 
     @Test
@@ -56,11 +65,26 @@ class DigitaliserDokumentStegTest {
         every { journalpost.kanBehandlesAutomatisk() } returns true
         every { struktureringsvurderingRepository.hentStruktureringsavklaring(any()) } returns null
         every { journalpostRepo.hentHvisEksisterer(any<BehandlingId>()) } returns journalpost
+        every { kategorivurderingRepo.hentKategoriAvklaring(any()) } returns mockk(relaxed = true)
 
         val stegresultat = digitaliserDokumentSteg.utfør(mockk(relaxed = true))
 
-        assertThat(stegresultat is Fullført)
+        assertEquals(stegresultat::class.simpleName, Fullført::class.simpleName)
 
+    }
+    
+    @Test
+    fun `når behandlingen har kategori som ikke skal struktureres forventes ingen avklaringsbehov`() {
+        val journalpost: Journalpost = mockk(relaxed = true)
+
+        every { journalpost.kanBehandlesAutomatisk() } returns false
+        every { struktureringsvurderingRepository.hentStruktureringsavklaring(any()) } returns null
+        every { journalpostRepo.hentHvisEksisterer(any<BehandlingId>()) } returns journalpost
+        every { kategorivurderingRepo.hentKategoriAvklaring(any()) } returns KategoriVurdering(InnsendingType.LEGEERKLÆRING)
+
+        val stegresultat = digitaliserDokumentSteg.utfør(mockk(relaxed = true))
+
+        assertEquals(stegresultat::class.simpleName, Fullført::class.simpleName)
     }
 
 }
