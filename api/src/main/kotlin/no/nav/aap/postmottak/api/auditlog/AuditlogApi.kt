ace4212880ee2ff1c1df24f7f1a16b11965578dd
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-02-10 11:09:15 +0100

    :bug: Korrekt path param

--- api/src/main/kotlin/no/nav/aap/postmottak/api/auditlog/AuditlogApi.kt
+++ api/src/main/kotlin/no/nav/aap/postmottak/api/auditlog/AuditlogApi.kt
@@ -4,6 +4,7 @@ import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.response.respondWithStatus
 import com.papsign.ktor.openapigen.route.route
 import io.ktor.http.*
+import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
 import no.nav.aap.tilgang.AuthorizationParamPathConfig
 import no.nav.aap.tilgang.JournalpostPathParam
 import no.nav.aap.tilgang.authorizedPost
@@ -11,11 +12,11 @@ import javax.sql.DataSource
 
 fun NormalOpenAPIRoute.auditlogApi(dataSource: DataSource) {
     route("/api/journalpost") {
-        route("/{journalpostId}/auditlog") {
-            authorizedPost<JournalpostPathParam, Unit, Unit>(
+        route("/{referanse}/auditlog") {
+            authorizedPost<JournalpostId, Unit, Unit>(
                 AuthorizationParamPathConfig(
                     journalpostPathParam = JournalpostPathParam(
-                        "journalpostId"
+                        "referanse"
                     )
                 ),
                 DefaultAuditLogConfig.fraJournalpostPathParam("journalpostId", dataSource)
