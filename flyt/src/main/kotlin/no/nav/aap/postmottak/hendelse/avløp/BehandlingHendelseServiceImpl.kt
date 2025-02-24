e5190fce25b8ec4f70da428398810674c593f776
Fredrik Meyer <fredrik.meyer@nav.no>
2025-02-24 08:20:31 +0100

    Fjern deprekert felt

--- flyt/src/main/kotlin/no/nav/aap/postmottak/hendelse/avløp/BehandlingHendelseServiceImpl.kt
+++ flyt/src/main/kotlin/no/nav/aap/postmottak/hendelse/avløp/BehandlingHendelseServiceImpl.kt
@@ -7,9 +7,7 @@ import no.nav.aap.postmottak.avklaringsbehov.Avklaringsbehovene
 import no.nav.aap.postmottak.avklaringsbehov.løser.ÅrsakTilSettPåVent
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.JournalpostRepository
 import no.nav.aap.postmottak.journalpostogbehandling.behandling.Behandling
-import no.nav.aap.postmottak.kontrakt.avklaringsbehov.Status
 import no.nav.aap.postmottak.kontrakt.hendelse.AvklaringsbehovHendelseDto
-import no.nav.aap.postmottak.kontrakt.hendelse.DefinisjonDTO
 import no.nav.aap.postmottak.kontrakt.hendelse.DokumentflytStoppetHendelse
 import no.nav.aap.postmottak.kontrakt.hendelse.EndringDTO
 import no.nav.aap.postmottak.prosessering.StoppetHendelseJobbUtfører
@@ -36,17 +34,8 @@ class BehandlingHendelseServiceImpl(
             avklaringsbehov = avklaringsbehovene.alle().map { avklaringsbehov ->
                 AvklaringsbehovHendelseDto(
                     avklaringsbehovDefinisjon = avklaringsbehov.definisjon,
-                    definisjon = DefinisjonDTO(
-                        type = avklaringsbehov.definisjon.kode,
-                        behovType = avklaringsbehov.definisjon.type,
-                        løsesISteg = avklaringsbehov.løsesISteg()
-                    ),
                     status = avklaringsbehov.status(),
-                    endringer = avklaringsbehov.historikk.filter {
-                        it.status in listOf(
-                            Status.OPPRETTET, Status.SENDT_TILBAKE_FRA_BESLUTTER, Status.AVSLUTTET
-                        )
-                    }.map { endring ->
+                    endringer = avklaringsbehov.historikk.map { endring ->
                         EndringDTO(
                             status = endring.status,
                             tidsstempel = endring.tidsstempel,
--- kontrakt/src/main/kotlin/no/nav/aap/postmottak/kontrakt/hendelse/AvklaringsbehovHendelseDto.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/postmottak/kontrakt/hendelse/AvklaringsbehovHendelseDto.kt
@@ -1,24 +1,14 @@
 package no.nav.aap.postmottak.kontrakt.hendelse
 
-import no.nav.aap.postmottak.kontrakt.avklaringsbehov.AvklaringsbehovKode
 import no.nav.aap.postmottak.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.postmottak.kontrakt.avklaringsbehov.Status
-import no.nav.aap.postmottak.kontrakt.steg.StegType
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class AvklaringsbehovHendelseDto(
-    @Deprecated(message = "Bruk direkte definisjon.")
-    val definisjon: DefinisjonDTO?,
     val avklaringsbehovDefinisjon: Definisjon,
     val status: Status,
     val endringer: List<EndringDTO>
-)
-
-data class DefinisjonDTO(
-    val type: AvklaringsbehovKode,
-    val behovType: Definisjon.BehovType,
-    val løsesISteg: StegType
 )
 
 data class EndringDTO(
