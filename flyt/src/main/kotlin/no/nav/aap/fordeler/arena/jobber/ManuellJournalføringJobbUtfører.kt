2694bf61b91be82ed1b4443c888818186e9e8429
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-03-03 20:28:52 +0100

    Håndter journalposter som feilaktig fikk fordelingsoppgave
    co-authored-by: Ine Lofthus Arnesen <Ine.Lofthus.Arnesen@nav.no>

--- flyt/src/main/kotlin/no/nav/aap/fordeler/arena/jobber/ManuellJournalføringJobbUtfører.kt
+++ flyt/src/main/kotlin/no/nav/aap/fordeler/arena/jobber/ManuellJournalføringJobbUtfører.kt
@@ -14,6 +14,7 @@ import no.nav.aap.postmottak.gateway.GosysOppgaveGateway
 import no.nav.aap.postmottak.gateway.Oppgavetype
 import no.nav.aap.postmottak.journalføringCounter
 import no.nav.aap.postmottak.journalpostogbehandling.journalpost.JournalpostMedDokumentTitler
+import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
 import org.slf4j.LoggerFactory
 
 private val log = LoggerFactory.getLogger(ManuellJournalføringJobbUtfører::class.java)
@@ -53,6 +54,55 @@ class ManuellJournalføringJobbUtfører(
                 kontekst.journalpostId,
                 listOf(Oppgavetype.JOURNALFØRING, Oppgavetype.FORDELING)
             )
+
+        /* midlertidig kode for å fikse fordelingsoppgaver. */
+        val specialCase = listOf(
+            698581045L,
+            698581047L,
+            698588247L,
+            698590382L,
+            698592673L,
+            698598590L,
+            698598613L,
+            698598639L,
+            698599024L,
+            698600148L,
+            698600159L,
+            698600240L,
+            698600312L,
+        ).map { JournalpostId(it) }
+
+        if (kontekst.journalpostId in specialCase) {
+            log.info("special case ${kontekst.journalpostId}")
+            val eksisterendeFordelingsOppgaver =
+                gosysOppgaveGateway.finnOppgaverForJournalpost(
+                    kontekst.journalpostId,
+                    listOf(Oppgavetype.FORDELING)
+                )
+            eksisterendeFordelingsOppgaver.forEach {
+                gosysOppgaveGateway.ferdigstillOppgave(it)
+            }
+            if (kontekst.navEnhet != null && input.antallRetriesForsøkt() < 3) {
+                gosysOppgaveGateway.opprettJournalføringsOppgave(
+                    kontekst.journalpostId,
+                    kontekst.ident,
+                    kontekst.hoveddokumenttittel,
+                    kontekst.navEnhet
+                )
+                log.info("Opprettet journalføringsoppgave i gosys for ${kontekst.journalpostId}")
+                prometheus.journalføringCounter(type = JournalføringsType.jfr, enhet = kontekst.navEnhet).increment()
+            } else {
+                gosysOppgaveGateway.opprettFordelingsOppgave(
+                    kontekst.journalpostId,
+                    kontekst.ident,
+                    kontekst.hoveddokumenttittel
+                )
+                log.info("Forsøkt å opprette journalføringsoppgave for journalpost ${kontekst.journalpostId} for mange ganger - opprettet fordelingsoppgave")
+                prometheus.journalføringCounter(type = JournalføringsType.fdr).increment()
+            }
+            return
+        }
+
         if (eksisterendeOppgaver.isNotEmpty()) {
             log.info("Det finnes allerede en journalføringsoppgave for journalpost ${kontekst.journalpostId} - oppretter ingen ny")
         } else if (kontekst.navEnhet != null && input.antallRetriesForsøkt() < 3) {
