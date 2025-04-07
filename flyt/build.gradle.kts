47ca54c8fa44aaacc5e51680d8f2c07349193405
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-04-07 20:34:19 +0200

    Bump felleskomponenter til 1.0.202

--- flyt/build.gradle.kts
+++ flyt/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("postmottak.conventions")
 }
 
-val komponenterVersjon = "1.0.200"
+val komponenterVersjon = "1.0.202"
 val behandlingsflytVersjon = "0.0.197"
 val ktorVersion = "3.1.2"
 val tilgangVersjon = "1.0.39"
