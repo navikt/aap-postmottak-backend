bb61707ea53cb615ff3b42595befb821f2547a0a
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-04-22 11:02:00 +0200

    feil exception oppdatert :))

--- flyt/src/main/kotlin/no/nav/aap/postmottak/avklaringsbehov/AvslagException.kt
+++ flyt/src/main/kotlin/no/nav/aap/postmottak/avklaringsbehov/AvslagException.kt
@@ -7,7 +7,7 @@ import no.nav.aap.postmottak.exception.FlytOperasjonException
 class AvslagException : FlytOperasjonException,
     RuntimeException("Fikk innkommende dokument på tidligere avslått sak, mangler håndtering av dette") {
     override fun status(): HttpStatusCode {
-        return HttpStatusCode.Conflict
+        return HttpStatusCode.NotImplemented
     }
 
     override fun body(): ErrorRespons {
