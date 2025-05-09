b77605b3e359ed44497ad9c7476d9cc133e0de8d
Henrik Gundersen <henrik.gundersen@nav.no>
2025-05-09 15:16:20 +0200

    AAP-903 Setter saksnummer hvis sak finnes for oppgave

--- flyt/src/main/kotlin/no/nav/aap/postmottak/hendelse/mottak/BehandlingHendelseHåndterer.kt
+++ flyt/src/main/kotlin/no/nav/aap/postmottak/hendelse/mottak/BehandlingHendelseHåndterer.kt
@@ -1,10 +1,12 @@
 package no.nav.aap.postmottak.hendelse.mottak
 
 import no.nav.aap.komponenter.dbconnect.DBConnection
+import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.motor.FlytJobbRepository
 import no.nav.aap.postmottak.avklaringsbehov.AvklaringsbehovOrkestrator
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.JournalpostRepository
+import no.nav.aap.postmottak.gateway.BehandlingsflytGateway
 import no.nav.aap.postmottak.hendelse.avløp.BehandlingHendelseServiceImpl
 import no.nav.aap.postmottak.journalpostogbehandling.behandling.BehandlingId
 
@@ -14,7 +16,8 @@ class BehandlingHendelseHåndterer(connection: DBConnection) {
         connection,
         BehandlingHendelseServiceImpl(
             FlytJobbRepository(connection),
-            RepositoryProvider(connection).provide(JournalpostRepository::class)
+            RepositoryProvider(connection).provide(JournalpostRepository::class),
+            GatewayProvider.provide(BehandlingsflytGateway::class),
         )
     )
 
