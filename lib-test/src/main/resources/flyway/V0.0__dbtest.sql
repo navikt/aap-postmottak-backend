e2d6123795f45ed41808457f7cc603d5a060b87a
tobiasmcvey <tobias.mcvey@gmail.com>
2025-05-27 08:22:20 +0200

    legger til servicebruker for innsikt og målinger (#205)
    * legger til servicebruker for innsikt og målinger
    
    * Lage cloudsqliamserviceaccount for testdb
    
    ---------
    
    Co-authored-by: Fredrik Meyer <fredrik.meyer@nav.no>

--- lib-test/src/main/resources/flyway/V0.0__dbtest.sql
+++ lib-test/src/main/resources/flyway/V0.0__dbtest.sql
@@ -1,3 +1,6 @@
 -- V0.0__dbtest.sql brukes til å opprette ting som finnes på GCP.
 DROP ROLE IF EXISTS cloudsqliamuser;
 CREATE ROLE cloudsqliamuser;
+
+DROP ROLE IF EXISTS cloudsqliamserviceaccount;
+CREATE ROLE cloudsqliamserviceaccount;
