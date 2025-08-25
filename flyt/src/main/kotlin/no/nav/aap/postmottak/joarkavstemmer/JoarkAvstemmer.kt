d0c030ffb7ba93721f2161f637ddd6c0eeed4c4c
Fredrik Meyer <fredrik.meyer@nav.no>
2025-08-25 10:08:40 +0200

    Logg info i dev

--- flyt/src/main/kotlin/no/nav/aap/postmottak/joarkavstemmer/JoarkAvstemmer.kt
+++ flyt/src/main/kotlin/no/nav/aap/postmottak/joarkavstemmer/JoarkAvstemmer.kt
@@ -11,9 +11,7 @@ import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
 import no.nav.aap.unleash.PostmottakFeature
 import no.nav.aap.unleash.UnleashGateway
 import org.slf4j.LoggerFactory
-import java.time.LocalDate
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
+import org.slf4j.event.Level
 
 /**
  * Prøver å replikere logikken her: https://confluence.adeo.no/spaces/BOA/pages/366859456/doksikkerhetsnett
@@ -38,37 +36,26 @@ class JoarkAvstemmer(
         }
     }
 
+    private fun loggUavstemt(melding: String) {
+        val level = if (Miljø.erProd()) Level.ERROR else Level.INFO
+        return log.makeLoggingEventBuilder(level).setMessage(melding).log()
+    }
+
     private fun avstemJournalPost(journalpost: JournalpostFraDoksikkerhetsnett) {
         val journalpostId = JournalpostId(journalpost.journalpostId)
         val regelResultat = regelRepository.hentRegelresultat(journalpostId)
 
-        if (journalpost.datoOpprettet.isBefore(
-                OffsetDateTime.of(
-                    LocalDate.of(2025, 4, 1).atStartOfDay(),
-                    ZoneOffset.UTC
-                )
-            ) && !Miljø.erProd()
-        ) {
-            gosysOppgaveGateway.opprettJournalføringsOppgaveHvisIkkeEksisterer(
-                journalpostId = journalpostId,
-                personIdent = null,
-                beskrivelse = "Automatisk gjenopprettet oppgave",
-                tildeltEnhetsnr = tildeltEnhetsnr(journalpost.journalforendeEnhet),
-                behandlingstema = journalpost.behandlingstema,
-            )
-        }
-
         if (regelResultat == null) {
-            log.error("Fant ikke regelresultat for journalpostId=$journalpostId. Har ikke nok informasjon til å fullføre. Dato opprettet: ${journalpost.datoOpprettet}.")
+            loggUavstemt("Fant ikke regelresultat for journalpostId=$journalpostId. Har ikke nok informasjon til å fullføre. Dato opprettet: ${journalpost.datoOpprettet}.")
             return
         }
 
         if (regelResultat.gikkTilKelvin()) {
-            log.error("Fant ubehandlet journalpost eldre enn 5 dager som skal til Kelvin. ID: ${journalpostId}. Dato opprettet: ${journalpost.datoOpprettet}.")
+            loggUavstemt("Fant ubehandlet journalpost eldre enn 5 dager som skal til Kelvin. ID: ${journalpostId}. Dato opprettet: ${journalpost.datoOpprettet}.")
         } else {
             val uthentet = journalpostGateway.hentJournalpost(journalpostId)
             val ident = uthentet.bruker?.id
-            log.info("Fant ubehandlet journalpost. Oppretter Gosys-oppgave. JournalpostId: ${journalpostId}. Systemnavn: ${regelResultat.systemNavn}")
+            log.info("Fant ubehandlet journalpost som ikke skal til Kelvin. Oppretter Gosys-oppgave. JournalpostId: ${journalpostId}. Systemnavn: ${regelResultat.systemNavn}")
 
             if (unleashGateway.isEnabled(PostmottakFeature.AvstemMotJoark)) {
                 gosysOppgaveGateway.opprettJournalføringsOppgaveHvisIkkeEksisterer(
