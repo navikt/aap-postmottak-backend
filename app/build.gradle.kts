aa93be0ef9501e0c596077c73f6817415ad2e552
Kristian Laskemoen <61700528+L145025@users.noreply.github.com>
2025-09-08 14:55:29 +0200

    Update build.gradle.kts med nyeste teamdokumenthandtering-avro-schemas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     implementation("org.apache.avro:avro:1.12.0")
 
     implementation("io.confluent:kafka-streams-avro-serde:8.0.0")
-    implementation("no.nav.teamdokumenthandtering:teamdokumenthandtering-avro-schemas:08271806")
+    implementation("no.nav.teamdokumenthandtering:teamdokumenthandtering-avro-schemas:1.1.6")
 
     testImplementation(libs.kafkaStreamsTestUtils)
 
