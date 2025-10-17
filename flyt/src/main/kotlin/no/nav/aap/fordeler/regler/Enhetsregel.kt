30eb4db85e13c62edd3ff1aaa08badd9a80b8032
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2025-10-17 12:03:12 +0200

    Rydd opp etter oppskalering

--- flyt/src/main/kotlin/no/nav/aap/fordeler/regler/Enhetsregel.kt
+++ flyt/src/main/kotlin/no/nav/aap/fordeler/regler/Enhetsregel.kt
@@ -5,7 +5,6 @@ import no.nav.aap.fordeler.Enhetsutreder
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.postmottak.kontrakt.enhet.GodkjentEnhet
-import no.nav.aap.unleash.PostmottakFeature
 import no.nav.aap.unleash.UnleashGateway
 import org.slf4j.LoggerFactory
 
@@ -14,59 +13,6 @@ private val log = LoggerFactory.getLogger(Enhetsregel::class.java)
 class Enhetsregel(
     unleashGateway: UnleashGateway
 ) : Regel<EnhetsregelInput> {
-
-    private val godkjenteEnheter =
-        if (unleashGateway.isEnabled(PostmottakFeature.Oppskalering13oktober)) {
-            GodkjentEnhet.entries
-        } else {
-            listOf(
-                // Vest-Viken
-                GodkjentEnhet.NAV_BÆRUM,
-                GodkjentEnhet.NAV_ASKER,
-                GodkjentEnhet.NAV_JEVNAKER,
-                GodkjentEnhet.NAV_DRAMMEN,
-                GodkjentEnhet.NAV_KONGSBERG,
-                GodkjentEnhet.NAV_HOLE,
-                GodkjentEnhet.NAV_HALLINGDAL,
-                GodkjentEnhet.NAV_MIDTBUSKERUD,
-                GodkjentEnhet.NAV_ØVRE_EIKER,
-                GodkjentEnhet.NAV_LIER,
-                GodkjentEnhet.NAV_NUMEDAL,
-                GodkjentEnhet.NAV_RINGERIKE,
-                GodkjentEnhet.EGEN_ANSATT_VEST_VIKEN,
-
-                // Innlandet
-                GodkjentEnhet.SYFA_INNLANDET,
-                GodkjentEnhet.NAV_KONGSVINGER,
-                GodkjentEnhet.NAV_HAMAR,
-                GodkjentEnhet.NAV_RINGSAKER,
-                GodkjentEnhet.NAV_LØTEN,
-                GodkjentEnhet.NAV_STANGE,
-                GodkjentEnhet.NAV_ODAL,
-                GodkjentEnhet.NAV_EIDSKOG,
-                GodkjentEnhet.NAV_SOLØR,
-                GodkjentEnhet.NAV_ELVERUM,
-                GodkjentEnhet.NAV_TRYSIL,
-                GodkjentEnhet.NAV_ÅMOT,
-                GodkjentEnhet.NAV_STORELVDAL,
-                GodkjentEnhet.NAV_ENGERDAL,
-                GodkjentEnhet.NAV_NORDØSTERDAL,
-                GodkjentEnhet.NAV_LILLEHAMMER_GAUSDAL,
-                GodkjentEnhet.NAV_GJØVIK,
-                GodkjentEnhet.NAV_LESJA_DOVRE,
-                GodkjentEnhet.NAV_LOM_SKJÅK,
-                GodkjentEnhet.NAV_VÅGÅ,
-                GodkjentEnhet.NAV_MIDTGUDBRANDSDAL,
-                GodkjentEnhet.NAV_SEL,
-                GodkjentEnhet.NAV_ØYER,
-                GodkjentEnhet.NAV_ØSTRE_TOTEN,
-                GodkjentEnhet.NAV_VESTRE_TOTEN,
-                GodkjentEnhet.NAV_HADELAND,
-                GodkjentEnhet.NAV_LAND,
-                GodkjentEnhet.NAV_VALDRES,
-                GodkjentEnhet.EGNE_ANSATTE_INNLANDET
-            )
-        }
 
     companion object : RegelFactory<EnhetsregelInput> {
         override val erAktiv = miljøConfig(prod = true, dev = false)
@@ -81,7 +27,7 @@ class Enhetsregel(
         if (input.enheter.norgEnhet == null && input.enheter.oppfølgingsenhet == null) {
             log.info("Fant ikke enheter for person")
         }
-        return (input.enheter.oppfølgingsenhet ?: input.enheter.norgEnhet) in godkjenteEnheter.map { it.enhetNr }
+        return (input.enheter.oppfølgingsenhet ?: input.enheter.norgEnhet) in GodkjentEnhet.entries.map { it.enhetNr }
     }
 
     override fun regelNavn(): String {
