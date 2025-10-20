41a59204cd6984bf41976b092951f654b333506e
Fredrik Meyer <fredrik.meyer@nav.no>
2025-10-20 08:15:30 +0200

    Fjerne litt ubrukt kode

--- lib-test/src/main/kotlin/no/nav/aap/postmottak/test/modell/TestPerson.kt
+++ lib-test/src/main/kotlin/no/nav/aap/postmottak/test/modell/TestPerson.kt
@@ -2,9 +2,7 @@ package no.nav.aap.postmottak.test.modell
 
 import no.nav.aap.postmottak.faktagrunnlag.register.personopplysninger.Fødselsdato
 import no.nav.aap.postmottak.journalpostogbehandling.Ident
-import no.nav.aap.postmottak.test.FiktivtNavnGenerator
 import no.nav.aap.postmottak.test.FødselsnummerGenerator
-import no.nav.aap.postmottak.test.PersonNavn
 import java.time.LocalDate
 
 fun genererIdent(fødselsdato: LocalDate): Ident {
@@ -14,10 +12,9 @@ fun genererIdent(fødselsdato: LocalDate): Ident {
 class TestPerson(
     val fødselsdato: Fødselsdato = Fødselsdato(LocalDate.now().minusYears(19)),
     val identer: Set<Ident> = setOf(genererIdent(fødselsdato.toLocalDate())),
-    val navn: PersonNavn = FiktivtNavnGenerator.genererNavn(),
     var uføre: Int? = null,
 ) {
     override fun toString(): String {
-        return "TestPerson(fødselsdato=$fødselsdato, identer=$identer, navn=$navn"
+        return "TestPerson(fødselsdato=$fødselsdato, identer=$identer"
     }
 }
