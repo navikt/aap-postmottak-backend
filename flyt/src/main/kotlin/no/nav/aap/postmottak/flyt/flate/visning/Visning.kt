b9968f2652b353484fd9ce240c6a58cc3cd063a9
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-10-30 12:43:33 +0100

    Eksponer behandlingstype i visning fra flytendepunktet

--- flyt/src/main/kotlin/no/nav/aap/postmottak/flyt/flate/visning/Visning.kt
+++ flyt/src/main/kotlin/no/nav/aap/postmottak/flyt/flate/visning/Visning.kt
@@ -1,6 +1,9 @@
 package no.nav.aap.postmottak.flyt.flate.visning
 
+import no.nav.aap.postmottak.kontrakt.behandling.TypeBehandling
+
 data class Visning(
     val visVentekort: Boolean,
     val readOnly: Boolean,
+    val typeBehandling: TypeBehandling
 )
