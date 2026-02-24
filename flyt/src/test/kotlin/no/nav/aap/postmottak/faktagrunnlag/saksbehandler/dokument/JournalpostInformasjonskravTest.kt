429f7ee996698ea18cf66afdf30a4864dfa8d501
Nicolas N. <nicolas.nordhagen@nav.no>
2026-02-24 09:21:46 +0100

    fix: delvis håndtere bruker = orgnr

--- flyt/src/test/kotlin/no/nav/aap/postmottak/faktagrunnlag/saksbehandler/dokument/JournalpostInformasjonskravTest.kt
+++ flyt/src/test/kotlin/no/nav/aap/postmottak/faktagrunnlag/saksbehandler/dokument/JournalpostInformasjonskravTest.kt
@@ -1,60 +1,86 @@
 package no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument
 
+import io.mockk.Runs
+import io.mockk.checkUnnecessaryStub
+import io.mockk.clearAllMocks
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import no.nav.aap.postmottak.faktagrunnlag.Informasjonskrav
+import no.nav.aap.postmottak.faktagrunnlag.register.PersonService
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.sak.SaksnummerRepository
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.sak.Saksvurdering
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.tema.AvklarTemaRepository
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.tema.Tema
 import no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.tema.TemaVurdering
+import no.nav.aap.postmottak.gateway.Bruker
+import no.nav.aap.postmottak.gateway.BrukerIdType
+import no.nav.aap.postmottak.gateway.JournalpostGateway
 import no.nav.aap.postmottak.gateway.Journalstatus
+import no.nav.aap.postmottak.gateway.SafDatoType
+import no.nav.aap.postmottak.gateway.SafJournalpost
+import no.nav.aap.postmottak.gateway.SafRelevantDato
+import no.nav.aap.postmottak.gateway.Sak
+import no.nav.aap.postmottak.gateway.Sakstype
 import no.nav.aap.postmottak.journalpostogbehandling.Ident
 import no.nav.aap.postmottak.journalpostogbehandling.behandling.BehandlingId
 import no.nav.aap.postmottak.journalpostogbehandling.behandling.dokumenter.KanalFraKodeverk
 import no.nav.aap.postmottak.journalpostogbehandling.flyt.FlytKontekst
-import no.nav.aap.postmottak.journalpostogbehandling.journalpost.Dokument
-import no.nav.aap.postmottak.journalpostogbehandling.journalpost.DokumentInfoId
-import no.nav.aap.postmottak.journalpostogbehandling.journalpost.Filtype
 import no.nav.aap.postmottak.journalpostogbehandling.journalpost.Journalpost
 import no.nav.aap.postmottak.journalpostogbehandling.journalpost.Person
-import no.nav.aap.postmottak.journalpostogbehandling.journalpost.Variant
-import no.nav.aap.postmottak.journalpostogbehandling.journalpost.Variantformat
 import no.nav.aap.postmottak.kontrakt.behandling.TypeBehandling
 import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.util.*
+import java.util.UUID
 
 class JournalpostInformasjonskravTest {
-    val journalpostService = mockk<JournalpostService>()
-    val journalpostRepository = mockk<JournalpostRepository>()
-    val saksnummerRepository = mockk<SaksnummerRepository>()
-    val avklarTemaRepository = mockk<AvklarTemaRepository>()
+    private val journalpostRepository = mockk<JournalpostRepository>()
+    private val saksnummerRepository = mockk<SaksnummerRepository>()
+    private val avklarTemaRepository = mockk<AvklarTemaRepository>()
+    private val journalpostGateway = mockk<JournalpostGateway>()
+    private val personService = mockk<PersonService>()
 
-    val journalpostInformasjonskrav = JournalpostInformasjonskrav(
+    private val person = Person(1, UUID.randomUUID(), listOf(Ident("12345678910")))
+
+    private val journalpostService = JournalpostService(journalpostGateway, personService)
+
+    private val journalpostInformasjonskrav = JournalpostInformasjonskrav(
         journalpostRepository,
         journalpostService,
         saksnummerRepository,
         avklarTemaRepository
     )
 
+    @BeforeEach
+    fun beforeEach() {
+        clearAllMocks()
+    }
+
+    @AfterEach
+    fun afterEach() {
+        checkUnnecessaryStub()
+    }
 
     @Test
     fun `Skal returnere ENDRET hvis saksnummer har blitt endret utenfor systemet`() {
         val journalpostId = JournalpostId(123456)
 
-        val journalpost = genererJournalpost(journalpostId)
-        val hentet = genererJournalpost(journalpostId, saksnummer = "1234")
+        val persistert = genererJournalpost(journalpostId)
+        val hentet = genererSafJournalpost(journalpostId, saksnummer = "1234")
 
         val behandlingId = BehandlingId(1)
         val flytKontekst = FlytKontekst(journalpostId, behandlingId, TypeBehandling.Journalføring)
 
-        every { journalpostService.hentJournalpost(journalpostId) } returns hentet
-        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns journalpost
-        every { journalpostRepository.lagre(hentet) } returns Unit
+        every { journalpostGateway.hentJournalpost(journalpostId) } returns hentet
+        every { personService.finnOgOppdaterPerson(any()) } returns person
+        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns persistert
+        every { journalpostRepository.lagre(any()) } just Runs
         every { saksnummerRepository.hentSakVurdering(behandlingId) } returns Saksvurdering("avklart saksnummer")
         every { avklarTemaRepository.hentTemaAvklaring(behandlingId) } returns null
 
@@ -66,17 +92,19 @@ class JournalpostInformasjonskravTest {
     fun `Skal returner ENDRET hvis tema har blitt endret utenfor systemet`() {
         val journalpostId = JournalpostId(123456)
 
-        val journalpost = genererJournalpost(journalpostId)
-        val hentet = genererJournalpost(journalpostId, tema = "nyttTema")
+        val persistert = genererJournalpost(journalpostId)
+        val hentet = genererSafJournalpost(journalpostId, tema = "nyttTema")
 
         val behandlingId = BehandlingId(1)
         val flytKontekst = FlytKontekst(journalpostId, behandlingId, TypeBehandling.Journalføring)
 
-        every { journalpostService.hentJournalpost(journalpostId) } returns hentet
-        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns journalpost
-        every { journalpostRepository.lagre(hentet) } returns Unit
+        every { journalpostGateway.hentJournalpost(journalpostId) } returns hentet
+        every { personService.finnOgOppdaterPerson(any()) } returns person
+        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns persistert
+        every { journalpostRepository.lagre(any()) } just Runs
         every { saksnummerRepository.hentSakVurdering(behandlingId) } returns null
         every { avklarTemaRepository.hentTemaAvklaring(behandlingId) } returns TemaVurdering(true, Tema.AAP)
+
         val actual = journalpostInformasjonskrav.oppdater(flytKontekst)
         assertThat(actual).isEqualTo(Informasjonskrav.Endret.ENDRET)
     }
@@ -84,17 +112,21 @@ class JournalpostInformasjonskravTest {
     @Test
     fun `Endringer som samsvarer med avklaringer skal returnere IKKE_ENDRET`() {
         val journalpostId = JournalpostId(123456)
-        val journalpost = genererJournalpost(journalpostId)
-        val hentet = genererJournalpost(journalpostId, saksnummer = "1234", tema = "AAP")
+        val saksvurdering = Saksvurdering("1234")
+
+        val persistert = genererJournalpost(journalpostId)
+        val hentet = genererSafJournalpost(journalpostId, saksnummer = saksvurdering.saksnummer, tema = "AAP")
 
         val behandlingId = BehandlingId(1)
         val flytKontekst = FlytKontekst(journalpostId, behandlingId, TypeBehandling.Journalføring)
 
-        every { journalpostService.hentJournalpost(journalpostId) } returns hentet
-        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns journalpost
-        every { journalpostRepository.lagre(hentet) } returns Unit
-        every { saksnummerRepository.hentSakVurdering(behandlingId) } returns Saksvurdering("1234")
+        every { journalpostGateway.hentJournalpost(journalpostId) } returns hentet
+        every { personService.finnOgOppdaterPerson(any()) } returns person
+        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns persistert
+        every { journalpostRepository.lagre(any()) } just Runs
+        every { saksnummerRepository.hentSakVurdering(behandlingId) } returns saksvurdering
         every { avklarTemaRepository.hentTemaAvklaring(behandlingId) } returns TemaVurdering(true, Tema.AAP)
+
         val actual = journalpostInformasjonskrav.oppdater(flytKontekst)
         assertThat(actual).isEqualTo(Informasjonskrav.Endret.IKKE_ENDRET)
     }
@@ -103,66 +135,95 @@ class JournalpostInformasjonskravTest {
     fun `Skal ikke returnere IKKE_ENDRET hvis journalposten er journalført, selv om det er andre relevante endringer`() {
         val journalpostId = JournalpostId(123456)
 
-        val journalpost = genererJournalpost(journalpostId)
-        val hentet = genererJournalpost(
+        val persistert = genererJournalpost(journalpostId)
+        val hentet = genererSafJournalpost(
             journalpostId,
             saksnummer = "1234",
             tema = "Annet tema",
             fagsystem = "Annet system",
-            status = Journalstatus.JOURNALFOERT
+            journalstatus = Journalstatus.JOURNALFOERT
         )
 
         val behandlingId = BehandlingId(1)
         val flytKontekst = FlytKontekst(journalpostId, behandlingId, TypeBehandling.Journalføring)
 
-        every { journalpostService.hentJournalpost(journalpostId) } returns hentet
-        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns journalpost
-        every { journalpostRepository.lagre(hentet) } returns Unit
-        every { saksnummerRepository.hentSakVurdering(behandlingId) } returns Saksvurdering("avklart saksnummer")
-        every { avklarTemaRepository.hentTemaAvklaring(behandlingId) } returns TemaVurdering(true, Tema.AAP)
+        every { journalpostGateway.hentJournalpost(journalpostId) } returns hentet
+        every { personService.finnOgOppdaterPerson(any()) } returns person
+        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns persistert
+        every { journalpostRepository.lagre(any()) } just Runs
 
         val actual = journalpostInformasjonskrav.oppdater(flytKontekst)
         assertThat(actual).isEqualTo(Informasjonskrav.Endret.IKKE_ENDRET)
     }
 
+    @Test
+    fun `Skal kaste feil hvis bruker er orgnr og status ikke er journafoert`() {
+        val journalpostId = JournalpostId(123456)
+
+        val hentet = genererSafJournalpost(
+            journalpostId,
+            Bruker(id = "987654321", BrukerIdType.ORGNR),
+            journalstatus = Journalstatus.MOTTATT
+        )
+
+        val behandlingId = BehandlingId(1)
+        val flytKontekst = FlytKontekst(journalpostId, behandlingId, TypeBehandling.Journalføring)
+
+        every { journalpostGateway.hentJournalpost(journalpostId) } returns hentet
+        every { journalpostRepository.hentHvisEksisterer(behandlingId) } returns genererJournalpost(journalpostId)
+
+        assertThrows<IllegalArgumentException> {
+            journalpostInformasjonskrav.oppdater(flytKontekst)
+        }
+    }
 
     private fun genererJournalpost(
         journalpostId: JournalpostId,
         person: Person = Person(1, UUID.randomUUID(), listOf(Ident("12345678901"))),
-        dokumenter: List<Dokument> = listOf(
-            Dokument(
-                DokumentInfoId("45426854351"),
-                "NAV 11.13-05",
-                "Dokumenttittel",
-                listOf(Variant(Filtype.PDF, Variantformat.ARKIV))
-            ),
-        ),
-        journalførendeEnhet: String? = null,
         tema: String = "AAP",
         status: Journalstatus = Journalstatus.MOTTATT,
-        kanal: KanalFraKodeverk = KanalFraKodeverk.UKJENT,
-        mottattDato: LocalDate = LocalDate.of(2020, 12, 1),
-        mottattTid: LocalDateTime = mottattDato.atStartOfDay(),
-        fagsystem: String? = null,
         saksnummer: String? = null,
-        behandlingstema: String? = null,
-        tittel: String? = null,
     ): Journalpost {
         return Journalpost(
             journalpostId = journalpostId,
             person = person,
             avsenderMottaker = null,
-            dokumenter = dokumenter,
-            journalførendeEnhet = journalførendeEnhet,
+            dokumenter = emptyList(),
+            journalførendeEnhet = null,
             tema = tema,
             status = status,
-            kanal = kanal,
-            mottattDato = mottattDato,
-            mottattTid = mottattTid,
-            fagsystem = fagsystem,
+            kanal = KanalFraKodeverk.NAV_NO,
+            mottattDato = LocalDate.now(),
+            mottattTid = null,
+            fagsystem = null,
             saksnummer = saksnummer,
-            behandlingstema = behandlingstema,
-            tittel = tittel
+            behandlingstema = null,
+            tittel = null,
+        )
+    }
+
+    private fun genererSafJournalpost(
+        journalpostId: JournalpostId,
+        bruker: Bruker = Bruker(id = "12345678910", BrukerIdType.FNR),
+        tema: String = "AAP",
+        journalstatus: Journalstatus = Journalstatus.MOTTATT,
+        kanal: KanalFraKodeverk = KanalFraKodeverk.UKJENT,
+        fagsystem: String? = null,
+        saksnummer: String? = null,
+    ): SafJournalpost {
+        return SafJournalpost(
+            journalpostId = journalpostId.referanse,
+            bruker = bruker,
+            dokumenter = emptyList(),
+            tema = tema,
+            journalstatus = journalstatus,
+            kanal = kanal,
+            relevanteDatoer = listOf(SafRelevantDato(LocalDateTime.now(), SafDatoType.DATO_REGISTRERT)),
+            sak = if (saksnummer != null || fagsystem != null) Sak(
+                fagsakId = saksnummer,
+                fagsaksystem = fagsystem,
+                sakstype = Sakstype.FAGSAK
+            ) else null,
         )
     }
 }
