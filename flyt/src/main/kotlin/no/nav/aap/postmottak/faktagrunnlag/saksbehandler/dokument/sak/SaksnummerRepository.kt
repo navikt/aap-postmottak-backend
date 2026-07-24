edf4659db1ee3a9224046d6d099c15fb2bd2f8f8
Nicolas N. <nicolas.nordhagen@nav.no>
2026-07-24 08:40:38 +0200

    AAP-2194 Hente saksnummer fra avklaring (driftApi) (#804)

--- flyt/src/main/kotlin/no/nav/aap/postmottak/faktagrunnlag/saksbehandler/dokument/sak/SaksnummerRepository.kt
+++ flyt/src/main/kotlin/no/nav/aap/postmottak/faktagrunnlag/saksbehandler/dokument/sak/SaksnummerRepository.kt
@@ -2,6 +2,7 @@ package no.nav.aap.postmottak.faktagrunnlag.saksbehandler.dokument.sak
 
 import no.nav.aap.lookup.repository.Repository
 import no.nav.aap.postmottak.journalpostogbehandling.behandling.BehandlingId
+import no.nav.aap.postmottak.kontrakt.journalpost.JournalpostId
 
 interface SaksnummerRepository: Repository {
     fun hentKelvinSaker(behandlingId: BehandlingId): List<Saksinfo>
@@ -11,5 +12,6 @@ interface SaksnummerRepository: Repository {
     )
     fun lagreSakVurdering(behandlingId: BehandlingId, saksvurdering: Saksvurdering)
     fun hentSakVurdering(behandlingId: BehandlingId): Saksvurdering?
+    fun hentSaksnummerForJournalpost(journalpostId: JournalpostId): String?
     fun eksistererAvslagPåTidligereBehandling(behandlingId: BehandlingId): Boolean
 }
