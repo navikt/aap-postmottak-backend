cbc7810af8452bbb072beb51e21e51f3e8f6a4ef
Fredrik Meyer <fredrik.meyer@nav.no>
2026-09-21 08:03:08 +0200

    Fiks test

--- flyt/src/test/kotlin/no/nav/aap/postmottak/faktagrunnlag/saksbehandler/dokument/sak/SaksinfoTest.kt
+++ flyt/src/test/kotlin/no/nav/aap/postmottak/faktagrunnlag/saksbehandler/dokument/sak/SaksinfoTest.kt
@@ -14,38 +14,36 @@ class SaksinfoTest {
     }
 
     @Test
-    fun `tillater automatisk behandling når det finnes en åpen sak, selv om alle avsluttede saker er avslag`() {
+    fun `tillater automatisk behandling når det finnes en åpen sak, selv om ingen saker har rett nå eller i fremtiden`() {
         val saker = listOf(
-            sak(avslag = true, finnesÅpenBehandling = true, harRettNåEllerIFramtiden = null),
+            sak(finnesÅpenBehandling = true, harRettNåEllerIFramtiden = null),
         )
         assertTrue(saker.tillaterAutomatiskBehandlingAvLegeerklæring())
     }
 
     @Test
-    fun `tillater automatisk behandling når det finnes en sak uten avslag`() {
+    fun `tillater automatisk behandling når det finnes en sak med rett nå eller i fremtiden`() {
         val saker = listOf(
-            sak(avslag = false, finnesÅpenBehandling = false, harRettNåEllerIFramtiden = null),
+            sak(finnesÅpenBehandling = false, harRettNåEllerIFramtiden = true),
         )
         assertTrue(saker.tillaterAutomatiskBehandlingAvLegeerklæring())
     }
 
     @Test
-    fun `tillater ikke automatisk behandling når alle saker er avslag og ingen åpen sak finnes`() {
+    fun `tillater ikke automatisk behandling når ingen sak har rett nå eller i fremtiden og ingen åpen sak finnes`() {
         val saker = listOf(
-            sak(avslag = true, finnesÅpenBehandling = false, harRettNåEllerIFramtiden = null),
-            sak(avslag = true, finnesÅpenBehandling = null, harRettNåEllerIFramtiden = null),
+            sak(finnesÅpenBehandling = false, harRettNåEllerIFramtiden = null),
+            sak(finnesÅpenBehandling = null, harRettNåEllerIFramtiden = null),
         )
         assertFalse(saker.tillaterAutomatiskBehandlingAvLegeerklæring())
     }
 
     private fun sak(
-        avslag: Boolean,
         finnesÅpenBehandling: Boolean?,
         harRettNåEllerIFramtiden: Boolean?,
     ) = Saksinfo(
         saksnummer = "42",
         periode = Periode(LocalDate.of(2020, 1, 1), LocalDate.of(2999, 12, 31)),
-        avslag = avslag,
         finnesÅpenBehandling = finnesÅpenBehandling,
         harRettNåEllerIFramtiden = harRettNåEllerIFramtiden,
     )
