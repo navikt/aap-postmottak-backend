04fc7cb77ef849060fefbde58569bbd0a8cb2304
Fredrik Meyer <fredrik.meyer@nav.no>
2026-09-25 12:46:42 +0200

    AAP-1727 Bruk beskrivelse fra begrunnelse i siste steget
    AAP-KONTOR-DOKUMENT

--- flyt/src/main/kotlin/no/nav/aap/postmottak/forretningsflyt/steg/dokumentflyt/OverleverTilFagsystemSteg.kt
+++ flyt/src/main/kotlin/no/nav/aap/postmottak/forretningsflyt/steg/dokumentflyt/OverleverTilFagsystemSteg.kt
@@ -1,8 +1,28 @@
 package no.nav.aap.postmottak.forretningsflyt.steg.dokumentflyt
 
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.InnsendingType
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.Aktivitetskort
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.AnnetRelevantDokument
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.AnnetRelevantDokumentV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.AnnetRelevantDokumentV1
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.ForeldrepengeVedtakV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.InstitusjonsOppholdHendelseV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.KabalHendelseV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.KlageV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.KorrigerSøknadsdatoV0
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.LegeerklæringV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.ManuellRevurderingV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.MeldekortV0
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.Melding
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.MigreringFraArenaV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.NyÅrsakTilBehandlingV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.OmgjøringKlageRevurdering
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.OppfølgingsoppgaveV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.PdlHendelseV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.SykepengevedtakV0
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.Søknad
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.TilbakekrevingHendelse
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.UførevedtakV0
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.postmottak.avklaringsbehov.AvklaringsbehovRepository
@@ -129,10 +149,55 @@ class OverleverTilFagsystemSteg(
             digitaliseringsvurdering.kategori == InnsendingType.LEGEERKLÆRING -> LegeerklæringV0(
                 beskrivelse = overleveringVurdering.begrunnelse
             )
+
             else -> DokumentTilMeldingParser.parseTilMelding(
                 digitaliseringsvurdering.strukturertDokument,
                 digitaliseringsvurdering.kategori
-            )
+            )?.let {
+                when (it) {
+                    is AnnetRelevantDokument -> {
+                        if (it.begrunnelse == null && overleveringVurdering.begrunnelse != null) when (it) {
+                            is AnnetRelevantDokumentV0 -> AnnetRelevantDokumentV1(
+                                årsakerTilBehandling = it.årsakerTilBehandling,
+                                begrunnelse = overleveringVurdering.begrunnelse,
+                                underkategori = null
+                            )
+
+                            is AnnetRelevantDokumentV1 -> it.copy(begrunnelse = overleveringVurdering.begrunnelse)
+                        } else it
+                    }
+
+                    is LegeerklæringV0 -> error("Skal ikke kunne skje, LegeerklæringV0 blir kun konstruert over.")
+                    is KlageV0 -> {
+                        if (it.beskrivelse.isBlank() && overleveringVurdering.begrunnelse != null) {
+                            it.copy(beskrivelse = overleveringVurdering.begrunnelse)
+                        } else it
+                    }
+
+                    is MeldekortV0 -> {
+                        if (it.begrunnelse.isNullOrBlank() && overleveringVurdering.begrunnelse != null) {
+                            it.copy(begrunnelse = overleveringVurdering.begrunnelse)
+                        } else it
+                    }
+
+                    // Disse har ikke begrunnelse / blir ikke digitalisert i postmottak
+                    is ManuellRevurderingV0,
+                    is Aktivitetskort,
+                    is ForeldrepengeVedtakV0,
+                    is InstitusjonsOppholdHendelseV0,
+                    is KabalHendelseV0,
+                    is KorrigerSøknadsdatoV0,
+                    is MigreringFraArenaV0,
+                    is NyÅrsakTilBehandlingV0,
+                    is OmgjøringKlageRevurdering,
+                    is OppfølgingsoppgaveV0,
+                    is PdlHendelseV0,
+                    is SykepengevedtakV0,
+                    is Søknad,
+                    is TilbakekrevingHendelse,
+                    is UførevedtakV0 -> it
+                }
+            }
         }
     }
 }
